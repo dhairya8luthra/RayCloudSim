a4b7ae303c637f4bee2441b949a3f6ed8d5a355e
reddy-j-harshith <reddy.j.harshith@gmail.com>
2025-02-18 22:20:51 +0530

    Modify the testcase gen logic

--- dataset_gen.py
+++ dataset_gen.py
@@ -3,10 +3,44 @@ import random
 import csv
 
 # Configuration
-NUM_TASKS = 100  # Adjust the number of tasks as needed
+NUM_TASKS = 100
 NODES = ["n0", "n1", "n2", "n3", "n4", "n5", "n6", "n7", "n8", "n9", "n10", "n11", "n12"]
+ONLINE_NODES = []
+ACTIVE_NODES = []
 
-# Generate task dataset
+def task_generator(env, writer):
+    task_id = 0
+    while task_id < NUM_TASKS:
+        pass
+    """
+    For each timestamp
+    -> All the nodes inintially are inactive
+
+    -> Gradually they become more active
+
+    Generation algorithm:
+    -> Generate the up times and the down times which describe which nodes are going go online and offline at each timestamp
+
+    -> Nodes that are actively doing a computation should not be turned off
+
+    -> Write the up nodes and down nodes to a json file into the format:
+    Example:
+    {
+        42.00: {
+            up_nodes: [n1, n2, n3],
+            down_nodes: [n4, n5]
+        },
+    }
+
+    Where 42.00 is the timestamp
+
+    -> For each timestamp, update the ONLINE_NODES list based on the new generated up times and the down times
+
+    -> Generate task dataset whose nodes are selected from the ONLINE_NODES list and then task is added to the dataset.
+    
+    """
+    pass
+
 def task_generator(env, writer):
     task_id = 0
     while task_id < NUM_TASKS:
