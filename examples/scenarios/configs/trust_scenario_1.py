587098a725f2c4213756ca9e7d56ec1522fc8e36
reddy-j-harshith <reddy.j.harshith@gmail.com>
2025-02-18 16:03:20 +0530

    Adding trust-based scenario

---
+++ examples/scenarios/configs/trust_scenario_1.py
@@ -0,0 +1,45 @@
+from core.base_scenario import BaseScenario
+from core.infrastructure import Node, Location
+
+from zoo import TrustNode, MaliciousNode
+
+
+class Scenario(BaseScenario):
+    """A Simple trust based-scenario"""
+
+    def init_infrastructure_nodes(self):
+        
+        no_of_nodes = len(self.json_nodes)
+
+        for node_info in self.json_nodes:
+
+            if node_info['NodeType'] == "TrustNode":
+                self.infrastructure.add_node(
+                    TrustNode(
+                        node_id=node_info['NodeId'], 
+                         name=node_info['NodeName'], 
+                         self_trust=1.0,
+                         max_cpu_freq=node_info['MaxCpuFreq'], 
+                         max_buffer_size=node_info['MaxBufferSize'], 
+                         location=Location(node_info['LocX'], node_info['LocY']),
+                         idle_energy_coef=node_info['IdleEnergyCoef'], 
+                         exe_energy_coef=node_info['ExeEnergyCoef']
+                    )
+                )
+            elif node_info['NodeType'] == "MaliciousNode":
+                malicious_node = MaliciousNode(
+                    node_id=node_info['NodeId'], 
+                     name=node_info['NodeName'], 
+                     self_trust=1.0,
+                     max_cpu_freq=node_info['MaxCpuFreq'], 
+                     max_buffer_size=node_info['MaxBufferSize'], 
+                     location=Location(node_info['LocX'], node_info['LocY']),
+                     idle_energy_coef=node_info['IdleEnergyCoef'], 
+                     exe_energy_coef=node_info['ExeEnergyCoef'],
+                )
+                malicious_node.trust_mat = {node['NodeName']: 0.0 if node['NodeName'] != node_info['NodeName'] else 1.0 for node in self.json_nodes}
+                self.infrastructure.add_node(malicious_node)
+            self.node_id2name[node_info['NodeId']] = node_info['NodeName']
+
+def status(self, node_name=None, link_args=None):
+    return
